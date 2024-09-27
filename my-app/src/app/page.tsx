f0aedd27300999f871fc9d428a8901290f224d96
samiullah <Samiiullaah.347@gmail.com>
2024-09-28 00:07:54 +0500

    My Hellow World project

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hellow World</h1>
+      <p>This is my first "Hellow World" project. In this project I'm using HTML tags and CSS for designing.</p>
+    </div>
+  );
+}
